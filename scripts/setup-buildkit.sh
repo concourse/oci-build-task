39dce15e010cf1d136410605c41f68698f7a8bc7
Charles Duffy <charles@dyfis.net>
2025-03-30 12:16:25 -0500

    General cleanup of shell bits:
    - Avoid `sed` use for functionality built into shell parameter expansion
    - Avoid piping into a `while read` loop, which introduces the surprising behaviors discussed in [BashFAQ 24](https://mywiki.wooledge.org/BashFAQ/024)
    - Switch from external command `which` to built-in, POSIX-standardized `command -v`
    - Avoid using pushd and popd (which are interactive-extension options, not guaranteed to be compiled into a noninteractive shell at all).
    
    With this applied, `shellcheck scripts/*` runs clean.
    
    Not yet addressed:
    
    - Failure to sanitize or escape strings substituted into JSON (base image does not provide jq or this would be trivial)
    - Use of [`set -e`](https://mywiki.wooledge.org/BashFAQ/105)

--- scripts/setup-buildkit.sh
+++ scripts/setup-buildkit.sh
@@ -1,21 +1,28 @@
-if ! which buildctl >/dev/null || ! which buildkitd >/dev/null; then
+#!/usr/bin/env bash
+# this is sourced, not executed; the shebang above is a hint for shellcheck and/or editors
+
+uname_arch=$(uname -m)
+case $uname_arch in
+  x86_64) arch=amd64;;
+  aarch64) arch=arm64;;
+  *) arch=$uname_arch;;
+esac
+
+if ! command -v buildctl >/dev/null || ! command -v buildkitd >/dev/null; then
   BUILDKIT_VERSION=0.9.1
-  BUILDKIT_URL=https://github.com/moby/buildkit/releases/download/v$BUILDKIT_VERSION/buildkit-v$BUILDKIT_VERSION.linux-amd64.tar.gz
+  BUILDKIT_URL=https://github.com/moby/buildkit/releases/download/v$BUILDKIT_VERSION/buildkit-v$BUILDKIT_VERSION.linux-${arch}.tar.gz
 
   curl -fL "$BUILDKIT_URL" | tar zxf -
 fi
 
-if [ "$(id -u)" != "0" ]; then
-  if ! which newuidmap >/dev/null || ! which newgidmap >/dev/null; then
-    echo "newuidmap and newgidmap must be installed"
+if [ "$UID" != "0" ]; then
+  if ! command -v newuidmap >/dev/null || ! command -v newgidmap >/dev/null; then
+    echo "newuidmap and newgidmap must be installed" >&2
     exit 1
   fi
 
-  if ! which rootlesskit >/dev/null; then
-    pushd rootlesskit
-      make
-    popd
-
+  if ! command -v rootlesskit >/dev/null; then
+    (cd rootlesskit && exec make)
     cp rootlesskit/bin/* bin/
   fi
 fi
