883ecd34586fce909e17965a5563947173440c73
Taylor Silva <dev@taydev.net>
2025-04-15 19:02:51 -0400

    enable scripts to run on other arch's
    I wanted to be able to run this on my arm Mac with Docker and didn't
    want to have to fiddle with the script each time.
    
    Signed-off-by: Taylor Silva <dev@taydev.net>

--- scripts/setup-buildkit.sh
+++ scripts/setup-buildkit.sh
@@ -1,6 +1,22 @@
+#!/usr/bin/env bash
+
 if ! which buildctl >/dev/null || ! which buildkitd >/dev/null; then
-  BUILDKIT_VERSION=0.17.2
-  BUILDKIT_URL=https://github.com/moby/buildkit/releases/download/v$BUILDKIT_VERSION/buildkit-v$BUILDKIT_VERSION.linux-amd64.tar.gz
+  arch="$(uname -m)"
+  case "$arch" in
+    "aarch64")
+      arch="arm64"
+      ;;
+
+    "x86_64")
+      arch="amd64"
+      ;;
+
+    *)
+      ;;
+  esac
+
+  BUILDKIT_VERSION="0.17.2"
+  BUILDKIT_URL="https://github.com/moby/buildkit/releases/download/v${BUILDKIT_VERSION}/buildkit-v${BUILDKIT_VERSION}.linux-${arch}.tar.gz"
 
   curl -fL "$BUILDKIT_URL" | tar zxf -
 fi
